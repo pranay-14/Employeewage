7e460d8969c03d6cb6644116b29fab6755cf6347
Venkat Pranai Likith Kunkalaguntla <vkunkala@in.ibm.com>
2020-06-16 00:23:49 +0530

    modified code of wage calculation

--- Employee.java
+++ Employee.java
@@ -2,21 +2,33 @@ import java.util.*;
 
 public class Employee
 {
-	public static void main(String args[])
-	{
-		int wagePerHour=20, workHours=8;
-		System.out.println("Welcome to Employee Wage calculation");
-		int isPresent=((int)(Math.random()*2 + 1)); 
-		if(isPresent==1)
-		{
-			System.out.println("Employee is Present");
-			int salary = wagePerHour * workHours;
-			System.out.println("Salary of Employee per day is : " + salary);
-		}
-		else
-		{
-			System.out.println("Employee is absent");
-			System.out.println("Salary cannot be calculated because employee is absent");
-		}
-	}
+        public static void main(String args[])
+        {
+                int wagePerHour=20, workHours=8;
+                int isFullTime=1;
+                System.out.println("Welcome to Employee Wage calculation");
+                int status=((int)(Math.random()*2 + 1));
+                if(status==1)
+                {
+                        System.out.println("Employee is Present");
+                        int jobType=((int)(Math.random()*2 + 1));
+                        if(jobType==isFullTime)
+                        {
+                                System.out.println("Employee is Full time Employee");
+                                int salary = wagePerHour * workHours;
+                                System.out.println("Salary of Employee per day is : " + salary);
+                        }
+                        else
+                        {
+                                System.out.println("Employee is not full time");
+                        }
+
+                }
+                else
+                {
+                        System.out.println("Employee is absent");
+                        System.out.println("Salary cannot be calculated because employee is absent");
+                }
+
+        }
 }
