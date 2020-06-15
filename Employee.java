590e266e8243a4afe95f20896aaa088d78efd570
Venkat Pranai Likith Kunkalaguntla <vkunkala@in.ibm.com>
2020-06-15 10:19:54 +0530

    Checking whether employee is present or absent

--- Employee.java
+++ Employee.java
@@ -1,7 +1,18 @@
+import java.util.*;
+
 public class Employee
 {
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage calculation");
+		int isPresent=((int)(Math.random()*2 + 1)); 
+		if(isPresent==1)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }
