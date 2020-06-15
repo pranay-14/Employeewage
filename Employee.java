375de3bf053659d0450753dcc388dfb2189eef37
Venkat Pranai Likith Kunkalaguntla <vkunkala@in.ibm.com>
2020-06-16 00:25:37 +0530

    a slight change of variable

--- Employee.java
+++ Employee.java
@@ -5,8 +5,8 @@ public class Employee
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage calculation");
-		int isPresent=((int)(Math.random()*2 + 1)); 
-		if(isPresent==1)
+		int status=((int)(Math.random()*2 + 1)); 
+		if(status==1)
 		{
 			System.out.println("Employee is Present");
 		}
